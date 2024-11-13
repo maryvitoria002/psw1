d27c41fbea256a433e07ec1c7ab7d604c8bbec77
Mary <146475589+maryvitoria002@users.noreply.github.com>
2024-11-13 16:41:45 -0300

    docker e php

---
+++ aula-13-11/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li> <a href="">Home</a> </li>
+            <li><a href="">News</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">About</a></li>
+           
+        </ul>
+    </nav>
+</body>
+</html>
